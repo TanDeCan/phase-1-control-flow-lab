31ba2d4a2a58131f0262404ea7c72474c5a86906
VictoriaBladow <vbladow9999@yahoo.com>
2022-12-19 17:25:03 -0600

    commit tree d19993762e9cc963c8c405d6e1fb482f998399d4

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ex) {
+  if (ex < 200){
+    return 'This one is on me!'
+  } else if (ex >= 200 && ex <= 2000){
+    return 'That will be twenty bucks.'
+  } else if (ex >=2001 && ex <=2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (ex >=2501){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous') {
+    return 'Thank you so much.'
+  } else if (tip === 'not as generous') {
+    return  'Thank you.'
+  } else {
+    return 'Bye.'
+  }
 }
